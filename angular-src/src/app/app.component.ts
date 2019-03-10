38e5d59c11c9e35114c8303a89f092d8c7189c28
p9595jh <42954589+p9595jh@users.noreply.github.com>
2019-03-10 20:16:36 +0900

    2019.03.10 20:16

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { NgFlashMessageService } from 'ng-flash-messages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-src in MEANAuthApp';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flashMessage: NgFlashMessageService
+    ) { }
+
+  onLogoutClick() {
+    this.authService.logout();
+    this.flashMessage.showFlashMessage({
+      messages: ['You are logged out'], 
+      type: 'success', 
+      timeout: 3000
+    });
+    this.router.navigate(['login']);
+    return false;
+  }
 }
